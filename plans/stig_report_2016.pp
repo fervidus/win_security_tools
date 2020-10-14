b1e04321a8346147630d9be7f4fc9065ce171933
Bryan Belanger <bryan@fervid.us>
2020-10-14 11:59:17 -0400

    Initial report

---
+++ plans/stig_report_2016.pp
@@ -0,0 +1,29 @@
+plan win_security_tools::stig_report_2016 (
+  TargetSpec $targets,
+) {
+  apply_prep($targets)
+
+  $results = apply($targets, _noop => true) {
+    package { 'nokogiri':
+      ensure   => installed,
+      provider => gem,
+    }
+
+    class { '::secure_windows': }
+  }
+
+  $report = $results[0].report
+
+  $statuses = parsejson($report['resource_statuses'])
+
+  $changes = $statuses.map | $resource, $value | {
+
+    {
+      resource => $resource,
+      message  => $value['events'][0]['message'],
+      stig     => grep($value['tags'], '^v\d+'),
+    }
+  }
+
+  return $report
+}
