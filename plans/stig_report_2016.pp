c511e0d02ddc8a57bc846b17e11235d8a9f1a8d7
Bryan Belanger <bryan@fervid.us>
2020-10-14 22:09:42 -0400

    Update stig_report_2016.pp

--- plans/stig_report_2016.pp
+++ plans/stig_report_2016.pp
@@ -24,7 +24,7 @@ plan win_security_tools::stig_report_2016 (
         undef   => '-',
         default => $value['events'][0]['message'],
       },
-      # stig     => grep($value['tags'], '^v\d+'),
+      stig     => grep($value['tags'], '^v\d+'),
     }
   }
 
